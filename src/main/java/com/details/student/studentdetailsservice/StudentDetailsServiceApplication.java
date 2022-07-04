783db5780eb3a65131744326a977346f0e025ced
rajasekhar reddy <puli.rajreddy@gmail.com>
2022-07-04 11:36:50 +0530

    first commit

---
+++ src/main/java/com/details/student/studentdetailsservice/StudentDetailsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.details.student.studentdetailsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentDetailsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentDetailsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/details/student/studentdetailsservice/StudentDetailsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.details.student.studentdetailsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentDetailsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
